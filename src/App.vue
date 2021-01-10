6147fb3ea9e4165a1585ca66293fc572748f9530
Megan Janes <Megan@megans-air-2.home>
2021-01-10 14:04:09 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <nav>
+   <router-link :to="{ name: 'Home'}">Home |</router-link> 
+  <router-link :to="{ name: 'About'}"> About</router-link>
+  </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; 
+}
+nav {
+  display: flex;
+  justify-content: flex-end;
+  padding: 1em;
+  background-color: #E5E7E9;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About the Developer</h1>
+    <p>Megan is a full stack developer, currently apprenticing as a product owner. This app was created for Megan and all her other friends and family who find themselves working out from home to support the endeavors of the front-line healthcare workers, including Megan's wife Tina and her cousin Carly.</p>
+    
   </div>
 </template>
+
+<script></script>
+<style></style>
